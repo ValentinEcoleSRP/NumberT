e0309d00757339020a5d00b4acd0d0a531769118
ValentinEcoleSRP <valentinleroy.ecolesrp@gmail.com>
2017-06-05 17:05:03 +0200

    php

--- header.php
+++ header.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-
   <head>
     <meta charset="utf-8">
     <meta name="description" content="Découvrez notre nouveau bar à thé">
@@ -12,17 +11,5 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond" rel="stylesheet">
     <title>Number T - Accueil</title>
-  </head>
-
-
-  <body>
-
-
-
-
-
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+    </head>
+    <body>
